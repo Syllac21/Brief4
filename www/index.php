527aab2b77f2f7aacb84c20299ecfa69c2635c20
theog48 <theo.grasset.dwwm48@gmail.com>
2025-02-19 13:48:34 +0100

    mise en place cards

--- www/index.php
+++ www/index.php
@@ -1,5 +1,12 @@
 <?php
 require_once 'src/model/Model.php';
+require_once 'src/model/Animaux.php'; // Inclusion du fichier Animaux.php
+
+// Création d'une instance de la classe Animaux
+$animauxModel = new Animaux();
+
+// Récupération des animaux depuis la base de données
+$animaux = $animauxModel->getAllAnimaux();
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -7,10 +14,9 @@ require_once 'src/model/Model.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <?php
-    phpinfo();
-    ?>
+
 </body>
 </html>

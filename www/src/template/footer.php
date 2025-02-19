dfc48d313fa4aacda310ba38075a067fe33acf38
Syllac21 <syllac.online@gmail.com>
2025-02-19 15:11:24 +0100

    update

---
+++ www/src/template/footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<style>
+.container{
+    background-color: #AFDFDF;
+    padding: 14px;
+}
+</style>
+
+
+<footer class="text-dark text-center custom-footer py-4 mt-5 ">
+    <div class="container">
+        <p class="mb-1">@2025 Refuge pour Animaux. WebNovices : Tous droits réservés.</p>
+    </div>
+</footer>
+    
+</body>
+</html>

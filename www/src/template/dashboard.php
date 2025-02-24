bb71c75f4ff541d05f74ec3d2695034ea21738b4
Syllac21 <syllac.online@gmail.com>
2025-02-24 15:33:18 +0100

    retrait du bouton page un employé

--- www/src/template/dashboard.php
+++ www/src/template/dashboard.php
@@ -34,7 +34,7 @@
     <a class="btn btn-outline-light" href="/?page=dashboard&table=animaux">Gestion des Animaux</a>
     <a class="btn btn-outline-light" href="/?page=dashboard&table=personnel">Gestion des Personnes</a>
     <a class="btn btn-outline-light" href="/?page=dashboard&table=espece">Gestion des Espèce</a>
-    <a class="btn btn-outline-light" href="/?page=dashboard&table=employe">Page un employé</a>git
+    <!-- <a class="btn btn-outline-light" href="/?page=dashboard&table=employe">Page un employé</a> -->
 </aside>
 
 <div class="content-wrapper">

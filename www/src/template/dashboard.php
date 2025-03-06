25f28d88045e6020c9ae4bcc2cf1086aa2c8d851
Syllac21 <syllac.online@gmail.com>
2025-03-06 15:03:00 +0100

    update

--- www/src/template/dashboard.php
+++ www/src/template/dashboard.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- Link to the external CSS file dashboard.css -->
-    <link rel="stylesheet" href="css/dashboardstyle.css">
-
-
-</head>
 <body>
     
 
